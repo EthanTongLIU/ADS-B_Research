1a6ee9cc83e13f1e2832fcfe2d784a482a0406d4
LiuTong <1142595791@qq.com>
2019-05-24 13:22:09 +0800

    update

--- false_alarm.m
+++ false_alarm.m
@@ -26,16 +26,30 @@ legend('f(u^{\prime})', 'f(u^{\prime\prime})');
 % plot(x, f3);
 
 figure;
-x0 = 0.1;
+x0 = 0.05;
 for j = 1 : 5
-  N = 5;
-  q = zeros(1, 200);
-  for i = 1 : 200
-    fun = @(y) 1 / sqrt(2 * pi) * sqrt(N / (4 * H)) * exp( - N * y .^ 2 / (8 * H) );
-    q(i) = integral(fun, x0, 10000);
-    N = N + 5;
-  end
-  plot(5 : 5 : 1000, q);
-  hold on;
-  x0 = x0 + 0.1;
+    N = 5;
+    q = zeros(1, 50);
+    for i = 1 : 50
+        fun = @(y) 1 / sqrt(2 * pi) * sqrt(N / (4 * H)) * exp( - N * y .^ 2 / (8 * H) );
+        q(i) = integral(fun, x0, 10000);
+        N = N + 20;
+    end
+    if j == 1
+        plot(5 : 20 : 1000, q, '-s', 'linewidth', 1, 'color', 'k');
+    elseif j == 2
+        plot(5 : 20 : 1000, q, '-d', 'linewidth', 1, 'color', 'k');
+    elseif j == 3
+        plot(5 : 20 : 1000, q, '-v', 'linewidth', 1, 'color', 'k');
+    elseif j == 4
+        plot(5 : 20 : 1000, q, '-^', 'linewidth', 1, 'color', 'k');
+    elseif j == 5
+        plot(5 : 20 : 1000, q, '-o', 'linewidth', 1, 'color', 'k');
+    end
+    hold on;
+    x0 = x0 + 0.05;
 end
+legend(['归一化门限', num2str(0.05)], ['归一化门限', num2str(0.1)], ['归一化门限', num2str(0.15)], ['归一化门限', num2str(0.2)], ['归一化门限', num2str(0.25)]);
+xlabel('单元个数', 'fontsize', 18);
+ylabel('虚警概率', 'fontsize', 18);
+title('虚警概率与单元个数和归一化门限的关系', 'fontsize', 18);

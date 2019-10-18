65768bd202b52946eeb7fa614f2879d1cc4aa4f8
LiuTong <1142595791@qq.com>
2019-10-18 22:25:19 +0800

    update

--- new_detection_proba.m
+++ new_detection_proba.m
@@ -13,34 +13,38 @@ hold on;
 
 r1 = 44; % 样本数，选定采样率为 22M
 
-SNR = 0.1;
+SNR = -10; % 信噪比，dB形式
+SNR = power(10, SNR / 10); % 信噪比，原始比例形式
 beta1 = linspace(0, 170, 100); % 检测门限
 Pd = 1 - normcdf(beta1 .* sqrt(pi/(2*r1)) - sqrt(r1) .* sqrt(SNR));
 plot(beta1, Pd, '-', 'color', [0 0 1], 'linewidth', 1.3);
 
-SNR = 0.5;
+SNR = -3; % 信噪比，dB形式
+SNR = power(10, SNR / 10); % 信噪比，原始比例形式
 beta1 = linspace(0, 170, 100); % 检测门限
 Pd = 1 - normcdf(beta1 .* sqrt(pi/(2*r1)) - sqrt(r1) .* sqrt(SNR));
 plot(beta1, Pd, '-.', 'color', [1 0 0], 'linewidth', 1.3);
 
-SNR = 1;
+SNR = 0; % 信噪比，dB形式
+SNR = power(10, SNR / 10); % 信噪比，原始比例形式
 beta1 = linspace(0, 170, 100); % 检测门限
 Pd = 1 - normcdf(beta1 .* sqrt(pi/(2*r1)) - sqrt(r1) .* sqrt(SNR));
 plot(beta1, Pd, ':', 'color', [1 0.65 0], 'linewidth', 1.3);
 
-SNR = 3;
+SNR = 3; % 信噪比，dB形式
+SNR = power(10, SNR / 10); % 信噪比，原始比例形式
 beta1 = linspace(0, 170, 100); % 检测门限
 Pd = 1 - normcdf(beta1 .* sqrt(pi/(2*r1)) - sqrt(r1) .* sqrt(SNR));
 plot(beta1, Pd, ':.', 'color', [1 0 1], 'linewidth', 1.3);
 
-SNR = 10;
+SNR = 10; % 信噪比，dB形式
+SNR = power(10, SNR / 10); % 信噪比，原始比例形式
 beta1 = linspace(0, 170, 100); % 检测门限
 Pd = 1 - normcdf(beta1 .* sqrt(pi/(2*r1)) - sqrt(r1) .* sqrt(SNR));
 plot(beta1, Pd, '--', 'color', [60/255 179/255 113/255], 'linewidth', 1.3);
 
-legend(['SNR = ', num2str(10*log10(0.1)), ' dB'], ['SNR = ', num2str(10*log10(0.5)), ' dB'],...
-    ['SNR = ', num2str(10*log10(1)), ' dB'], ['SNR = ', num2str(10*log10(3)), ' dB'], ['SNR = ', num2str(10*log10(10)), ' dB']);
-
+leg = legend(['-10', ' dB'], ['-3', ' dB'], ['0', ' dB'], ['3', ' dB'], ['10', ' dB']);
+title(leg, 'SNR');
 xlabel('$\beta_{1}$', 'interpreter', 'latex');
 ylabel('$P_{d}$', 'interpreter', 'latex');
 grid on;
@@ -77,8 +81,8 @@ SNR = linspace(0.1, 20, 1000); % 信噪比，原始比例形式
 Pd = 1 - normcdf(beta1 .* sqrt(pi/(2*r1)) - sqrt(r1) .* sqrt(SNR));
 plot(10*log10(SNR), Pd, '--', 'color', [60/255 179/255 113/255], 'linewidth', 1.3);
 
-legend('\beta_{1} = 20', '\beta_{1} = 30', '\beta_{1} = 40', '\beta_{1} = 50', '\beta_{1} = 60');
-
+leg = legend('20', '30', '40', '50', '60');
+title(leg, '\beta_{1}');
 xlabel('$SNR / dB$', 'interpreter', 'latex');
 ylabel('$P_{d}$', 'interpreter', 'latex');
 grid on;
--- new_error_detection_proba.m
+++ new_error_detection_proba.m
@@ -15,33 +15,93 @@ r2 = r0 - r1;
 
 figure;
 hold on;
-xlabel('\beta_{1}');
-ylabel('P_{e}');
 
 beta1 = linspace(0.1, 100, 100);
 
-for SNR = pi / 4 : pi / 4 : pi
-    beta2 = sqrt(2/pi) * r1 / r0 * sqrt(SNR) + r2 / r0;
-    Pe = 1 - normcdf(beta1 * beta2 * sqrt(pi/(2*r1)) - sqrt(r1) * sqrt(SNR));
-    plot(beta1, Pe, 'color', 'b');
-end
+SNR = -10; % 信噪比，dB形式
+SNR = power(10, SNR / 10); % 信噪比，原始比例形式
+beta2 = sqrt(2/pi) * r1 / r0 * sqrt(SNR) + r2 / r0;
+Pe = 1 - normcdf(beta1 * beta2 * sqrt(pi/(2*r1)) - sqrt(r1) * sqrt(SNR));
+plot(beta1, Pe, '-', 'color', [0 0 1], 'linewidth', 1.3);
+
+SNR = -3; % 信噪比，dB形式
+SNR = power(10, SNR / 10); % 信噪比，原始比例形式
+beta2 = sqrt(2/pi) * r1 / r0 * sqrt(SNR) + r2 / r0;
+Pe = 1 - normcdf(beta1 * beta2 * sqrt(pi/(2*r1)) - sqrt(r1) * sqrt(SNR));
+plot(beta1, Pe, '-.', 'color', [1 0 0], 'linewidth', 1.3);
+
+SNR = 0; % 信噪比，dB形式
+SNR = power(10, SNR / 10); % 信噪比，原始比例形式
+beta2 = sqrt(2/pi) * r1 / r0 * sqrt(SNR) + r2 / r0;
+Pe = 1 - normcdf(beta1 * beta2 * sqrt(pi/(2*r1)) - sqrt(r1) * sqrt(SNR));
+plot(beta1, Pe, ':', 'color', [1 0.65 0], 'linewidth', 1.3);
+
+SNR = 1.9612; % 信噪比，dB形式
+SNR = power(10, SNR / 10); % 信噪比，原始比例形式
+beta2 = sqrt(2/pi) * r1 / r0 * sqrt(SNR) + r2 / r0;
+Pe = 1 - normcdf(beta1 * beta2 * sqrt(pi/(2*r1)) - sqrt(r1) * sqrt(SNR));
+plot(beta1, Pe, '-..', 'color', 'k', 'linewidth', 1.0);
+
+SNR = 3; % 信噪比，dB形式
+SNR = power(10, SNR / 10); % 信噪比，原始比例形式
+beta2 = sqrt(2/pi) * r1 / r0 * sqrt(SNR) + r2 / r0;
+Pe = 1 - normcdf(beta1 * beta2 * sqrt(pi/(2*r1)) - sqrt(r1) * sqrt(SNR));
+plot(beta1, Pe, ':.', 'color', [1 0 1], 'linewidth', 1.3);
+
+SNR = 10; % 信噪比，dB形式
+SNR = power(10, SNR / 10); % 信噪比，原始比例形式
+beta2 = sqrt(2/pi) * r1 / r0 * sqrt(SNR) + r2 / r0;
+Pe = 1 - normcdf(beta1 * beta2 * sqrt(pi/(2*r1)) - sqrt(r1) * sqrt(SNR));
+plot(beta1, Pe, '--', 'color', [60/255 179/255 113/255], 'linewidth', 1.3);
+
+leg = legend(['-10', ' dB'], ['-3', ' dB'], ['0', ' dB'], ['1.96', 'dB'], ['3', ' dB'], ['10', ' dB']);
+title(leg, 'SNR');
+xlabel('$\beta_{1}$', 'interpreter', 'latex');
+ylabel('$P_{e}$', 'interpreter', 'latex');
+grid on;
 
 %% SNR 作为横轴
 
 figure;
 hold on;
-xlabel('SNR');
-ylabel('P_{e}');
 
-SNR = linspace(0.5, 5, 100);
+SNR = linspace(0.1, 20, 1000); % 信噪比，原始比例形式
 
-for beta1 = 0.1 : 10 : 100
-    beta2 = sqrt(2/pi) * r1 / r0 * sqrt(SNR) + r2 / r0;
-    Pe = 1 - normcdf(beta1 * beta2 * sqrt(pi/(2*r1)) - sqrt(r1) * sqrt(SNR));
-    plot(SNR, Pe, 'color', 'b');
-end
+beta1 = 20;
+beta2 = sqrt(2/pi) * r1 / r0 * sqrt(SNR) + r2 / r0;
+Pe = 1 - normcdf(beta1 * beta2 * sqrt(pi/(2*r1)) - sqrt(r1) * sqrt(SNR));
+plot(10*log10(SNR), Pe, '-', 'color', [0 0 1], 'linewidth', 1.3);
 
-plot([pi/2, pi/2], [0, 1], '-.', 'color', 'r');
+beta1 = 30;
+beta2 = sqrt(2/pi) * r1 / r0 * sqrt(SNR) + r2 / r0;
+Pe = 1 - normcdf(beta1 * beta2 * sqrt(pi/(2*r1)) - sqrt(r1) * sqrt(SNR));
+plot(10*log10(SNR), Pe, '-.', 'color', [1 0 0], 'linewidth', 1.3);
+
+SNR = linspace(0.1, 20, 100); % 信噪比，原始比例形式
+
+beta1 = 40;
+beta2 = sqrt(2/pi) * r1 / r0 * sqrt(SNR) + r2 / r0;
+Pe = 1 - normcdf(beta1 * beta2 * sqrt(pi/(2*r1)) - sqrt(r1) * sqrt(SNR));
+plot(10*log10(SNR), Pe, ':', 'color', [1 0.65 0], 'linewidth', 1.3);
+
+beta1 = 50;
+beta2 = sqrt(2/pi) * r1 / r0 * sqrt(SNR) + r2 / r0;
+Pe = 1 - normcdf(beta1 * beta2 * sqrt(pi/(2*r1)) - sqrt(r1) * sqrt(SNR));
+plot(10*log10(SNR), Pe, ':.', 'color', [1 0 1], 'linewidth', 1.3);
+
+beta1 = 60;
+beta2 = sqrt(2/pi) * r1 / r0 * sqrt(SNR) + r2 / r0;
+Pe = 1 - normcdf(beta1 * beta2 * sqrt(pi/(2*r1)) - sqrt(r1) * sqrt(SNR));
+plot(10*log10(SNR), Pe, '--', 'color', [60/255 179/255 113/255], 'linewidth', 1.3);
+
+plot([10*log10(pi/2), 10*log10(pi/2)], [0, 1], '-..', 'color', 'k'); % 临界信噪比
+
+leg = legend('20', '30', '40', '50', '60');
+title(leg, '\beta_{1}');
+set(gca, 'xtick', [-10 -5 0 1.96 5 10 15], 'xticklabel', {'-10', '-5', '0', '1.96', '5', '10', '15'});
+xlabel('$SNR / dB$', 'interpreter', 'latex');
+ylabel('$P_{e}$', 'interpreter', 'latex');
+grid on;
 
 
 

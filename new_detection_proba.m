6507e8f04d8df226c5cf81c958665da7c1a115d5
LiuTong <1142595791@qq.com>
2019-10-18 16:58:47 +0800

    update

--- new_detection_proba.m
+++ new_detection_proba.m
@@ -14,48 +14,74 @@ hold on;
 r1 = 44; % 样本数，选定采样率为 22M
 
 SNR = 0.1;
-beta1 = linspace(0, 140, 100); % 检测门限
+beta1 = linspace(0, 170, 100); % 检测门限
 Pd = 1 - normcdf(beta1 .* sqrt(pi/(2*r1)) - sqrt(r1) .* sqrt(SNR));
-plot(beta1, Pd, 'color', [0 0 1]);
+plot(beta1, Pd, '-', 'color', [0 0 1], 'linewidth', 1.3);
 
 SNR = 0.5;
-beta1 = linspace(0, 140, 100); % 检测门限
+beta1 = linspace(0, 170, 100); % 检测门限
 Pd = 1 - normcdf(beta1 .* sqrt(pi/(2*r1)) - sqrt(r1) .* sqrt(SNR));
-plot(beta1, Pd, 'color', [1 0 0]);
+plot(beta1, Pd, '-.', 'color', [1 0 0], 'linewidth', 1.3);
 
 SNR = 1;
-beta1 = linspace(0, 140, 100); % 检测门限
+beta1 = linspace(0, 170, 100); % 检测门限
 Pd = 1 - normcdf(beta1 .* sqrt(pi/(2*r1)) - sqrt(r1) .* sqrt(SNR));
-plot(beta1, Pd, 'color', [1 0.65 0]);
+plot(beta1, Pd, ':', 'color', [1 0.65 0], 'linewidth', 1.3);
 
 SNR = 3;
-beta1 = linspace(0, 140, 100); % 检测门限
+beta1 = linspace(0, 170, 100); % 检测门限
 Pd = 1 - normcdf(beta1 .* sqrt(pi/(2*r1)) - sqrt(r1) .* sqrt(SNR));
-plot(beta1, Pd, 'color', 'm');
+plot(beta1, Pd, ':.', 'color', [1 0 1], 'linewidth', 1.3);
 
 SNR = 10;
-beta1 = linspace(0, 140, 100); % 检测门限
+beta1 = linspace(0, 170, 100); % 检测门限
 Pd = 1 - normcdf(beta1 .* sqrt(pi/(2*r1)) - sqrt(r1) .* sqrt(SNR));
-plot(beta1, Pd, 'color', 'g');
+plot(beta1, Pd, '--', 'color', [60/255 179/255 113/255], 'linewidth', 1.3);
 
-xlabel('Threshold \beta_{1}');
-ylabel('P_{d}');
+legend(['SNR = ', num2str(10*log10(0.1)), ' dB'], ['SNR = ', num2str(10*log10(0.5)), ' dB'],...
+    ['SNR = ', num2str(10*log10(1)), ' dB'], ['SNR = ', num2str(10*log10(3)), ' dB'], ['SNR = ', num2str(10*log10(10)), ' dB']);
+
+xlabel('$\beta_{1}$', 'interpreter', 'latex');
+ylabel('$P_{d}$', 'interpreter', 'latex');
 grid on;
 
 %% 2. 固定r1，SNR 作为横轴
 
 figure;
 hold on;
-xlabel('SNR');
-ylabel('P_{d}');
 
-r1 = 30; % 样本数
+r1 = 44; % 样本数，采样率为 22M
 
-for beta1 = 20 : 10 : 60 % 检测门限
-    SNR = linspace(0.1, 20, 100); % 信噪比，原始比例形式
-    Pd = 1 - normcdf(beta1 .* sqrt(pi/(2*r1)) - sqrt(r1) .* sqrt(SNR));
-    plot(SNR, Pd, 'color', 'r');
-end
+beta1 = 20; % 检测门限
+SNR = linspace(0.1, 20, 1000); % 信噪比，原始比例形式
+Pd = 1 - normcdf(beta1 .* sqrt(pi/(2*r1)) - sqrt(r1) .* sqrt(SNR));
+plot(10*log10(SNR), Pd, '-', 'color', [0 0 1], 'linewidth', 1.3);
+
+beta1 = 30; % 检测门限
+SNR = linspace(0.1, 20, 1000); % 信噪比，原始比例形式
+Pd = 1 - normcdf(beta1 .* sqrt(pi/(2*r1)) - sqrt(r1) .* sqrt(SNR));
+plot(10*log10(SNR), Pd, '-.', 'color', [1 0 0], 'linewidth', 1.3);
+
+beta1 = 40; % 检测门限
+SNR = linspace(0.1, 20, 1000); % 信噪比，原始比例形式
+Pd = 1 - normcdf(beta1 .* sqrt(pi/(2*r1)) - sqrt(r1) .* sqrt(SNR));
+plot(10*log10(SNR), Pd, ':', 'color', [1 0.65 0], 'linewidth', 1.3);
+
+beta1 = 50; % 检测门限
+SNR = linspace(0.1, 20, 1000); % 信噪比，原始比例形式
+Pd = 1 - normcdf(beta1 .* sqrt(pi/(2*r1)) - sqrt(r1) .* sqrt(SNR));
+plot(10*log10(SNR), Pd, ':.', 'color', [1 0 1], 'linewidth', 1.3);
+
+beta1 = 60; % 检测门限
+SNR = linspace(0.1, 20, 1000); % 信噪比，原始比例形式
+Pd = 1 - normcdf(beta1 .* sqrt(pi/(2*r1)) - sqrt(r1) .* sqrt(SNR));
+plot(10*log10(SNR), Pd, '--', 'color', [60/255 179/255 113/255], 'linewidth', 1.3);
+
+legend('\beta_{1} = 20', '\beta_{1} = 30', '\beta_{1} = 40', '\beta_{1} = 50', '\beta_{1} = 60');
+
+xlabel('$SNR / dB$', 'interpreter', 'latex');
+ylabel('$P_{d}$', 'interpreter', 'latex');
+grid on;
 
 %% 3. 固定 beta1，r1 作为横轴
 
